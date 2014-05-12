5ce628ddfd3473c24cfc1243dad212b1b4948b1d
Mark Deaton <medeaton@hotmail.com>
2014-05-12 13:12:01 -0700

    Added config file

---
+++ config.js
@@ -0,0 +1,138 @@
+var config = {
+	"services": {
+		"basemap":"http://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Dark_Gray_Base_Beta/MapServer",
+		"cities" :"http://services.arcgis.com/6DIQcwlPy8knb6sg/arcgis/rest/services/HeatIndex_WM_2/FeatureServer/0"
+	},
+	"fieldmap": [
+	{"label"	: "2010",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]},
+	{"label"	: "2020",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]},
+	{"label"	: "2030",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]},
+	{"label"	: "2040",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]},
+	{"label"	: "2050",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]},
+	{"label"	: "2060",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]},
+	{"label"	: "2070",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]},
+	{"label"	: "2080",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]},
+	{"label"	: "2090",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]},
+	{"label"	: "2100",
+	 "fields"	: [
+	 	{"field" : "AvgJJA_HI", "fieldname" : "AvgJJA_HI"},
+	 	{"field" : "AvgSummer_HI", "fieldname" : "AvgSummer_HI"},
+	 	{"field" : "HtIdxCaut", "fieldname" : "HtIdxCaut"},
+	 	{"field" : "HtIdxECaut", "fieldname" : "HtIdxECaut"},
+	 	{"field" : "HtIdxDanger", "fieldname" : "HtIdxDanger"},
+	 	{"field" : "HtIdxEDanger", "fieldname" : "HtIdxEDanger"},
+	 	{"field" : "ConsecHtIdxCaut", "fieldname" : "ConsecHtIdxCaut"},
+	 	{"field" : "ConsecHtIdxECaut", "fieldname" : "ConsecHtIdxECaut"},
+	 	{"field" : "ConsecHtIdxDanger", "fieldname" : "ConsecHtIdxDanger"},
+	 	{"field" : "ConsecHtIdxEDanger", "fieldname" : "ConsecHtIdxEDanger"}
+	 ]}
+	]
+};
